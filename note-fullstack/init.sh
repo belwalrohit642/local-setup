f908fdc8ff5152cde3b0677c11fc6f62b7486407
belwalrohit642 <96739082+belwalrohit642@users.noreply.github.com>
2024-04-10 23:28:59 +0530

    Update init.sh

--- note-fullstack/init.sh
+++ note-fullstack/init.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-cd note-fullstack
 cd notes-ui
 npm install react react-dom react-router-dom react-scripts web-vitals
-npm start &
+nohup npm start &
 
 cd ../server
 npm install body-parser cross-env express mocha mysql2 nodemon sequelize should sqlite3 supertest
-npm start &
+nohup npm start &
 
 cd ../database
 service mysql start
 mysql -u root -psecret < notes.sql
-
+echo "CREATE USER 'rohit'@'localhost' IDENTIFIED BY 'secret';" | mysql -u root -psecret
+echo "GRANT ALL PRIVILEGES ON *.* TO 'rohit'@'localhost' WITH GRANT OPTION;" | mysql -u root -psecret
+echo "FLUSH PRIVILEGES;" | mysql -u root -psecret
