5749a8af21993a97b7137c9a8017410551671f5d
belwalrohit642 <96739082+belwalrohit642@users.noreply.github.com>
2024-04-11 11:54:03 +0530

    Update init.sh

--- note-fullstack/init.sh
+++ note-fullstack/init.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 cd notes-ui
 npm install react react-dom react-router-dom react-scripts web-vitals
-nohup npm start &
+#nohup npm start &
 
 cd ../server
 npm install body-parser cross-env express mocha mysql2 nodemon sequelize should sqlite3 supertest
 nohup npm start &
+
+cd ../notes-ui
+nohup npm start &
+
 
 cd ../database
 service mysql start
