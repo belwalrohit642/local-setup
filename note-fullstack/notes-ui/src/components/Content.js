cc44ce6182ce8c2624d12001deab9f94c99c4b67
belwalrohit642 <96739082+belwalrohit642@users.noreply.github.com>
2024-04-26 17:15:48 +0530

    Update Content.js

--- note-fullstack/notes-ui/src/components/Content.js
+++ note-fullstack/notes-ui/src/components/Content.js
@@ -14,7 +14,7 @@ class Content extends Component {
 
     componentDidMount() {
         this.setState({ isLoading: true })
-        let api_url = 'http://100.24.31.209:4006/notecontent?id='+this.props.match.params.id;
+        let api_url = process.env.REACT_APP_BACKEND_TABLE+'/notecontent?id='+this.props.match.params.id;
         fetch(api_url)
             .then(res => {
                 if(res.status >= 400) {
